fb66e2c5b1c804f2892b104ac28bfbc9a5b72e5d
victormgb <victormgb3099@gmail.com>
2020-08-21 16:47:06 -0400

    init project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `The Futures - World-class graphic design delivered in less than 24 hours`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@vessel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
